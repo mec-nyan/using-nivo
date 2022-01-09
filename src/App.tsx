903b33b6d4a62a66efe719200312e5cd331bd2b7
mec-nyan <marianoraspberrypi@gmail.com>
2022-01-08 22:56:14 -0300

    Add routing, template components (Home, More) and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,54 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
 import { MyResponsivePie, MyResponsiveBar } from "./components/Graphs";
 import { data, datum } from "./data";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Using Nivo Charts</h1>
-      </header>
-      <div className="charts">
-        <div className="container">
-          <MyResponsivePie data={data} />
-        </div>
-        <div className="container">
-          <MyResponsiveBar data={datum} />
-        </div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="pie" element={<Pie />} />
+        <Route path="bar" element={<Bar />} />
+        <Route path="more" element={<More />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className="temp">
+      <h2>Fucking home, man!</h2>
+    </div>
+  );
+}
+
+function More() {
+  return (
+    <div className="temp">
+      <h2>Want some more?</h2>
+    </div>
+  );
+}
+
+function Pie() {
+  return (
+    <div className="charts">
+      <div className="container">
+        <MyResponsivePie data={data} />
+      </div>
+    </div>
+  );
+}
+
+function Bar() {
+  return (
+    <div className="charts">
+      <div className="container">
+        <MyResponsiveBar data={datum} />
       </div>
     </div>
   );
